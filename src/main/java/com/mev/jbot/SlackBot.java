9eeb5abc93dcd42b576f114d674101fe2a13cee2
vgradilenko <volodymyr.gradilenko@mev.com>
2018-02-09 17:24:29 +0200

    init commit add basic functional

---
+++ src/main/java/com/mev/jbot/SlackBot.java
@@ -0,0 +1,39 @@
+package com.mev.jbot;
+
+import me.ramswaroop.jbot.core.slack.Bot;
+import me.ramswaroop.jbot.core.slack.Controller;
+import me.ramswaroop.jbot.core.slack.EventType;
+import me.ramswaroop.jbot.core.slack.models.Event;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketSession;
+
+@Component
+public class SlackBot extends Bot {
+
+    private final SlackBotService service;
+
+    @Autowired
+    public SlackBot(SlackBotService service) {
+        this.service = service;
+    }
+
+    @Value("${slackBotToken}")
+    private String slackToken;
+
+    @Override
+    public String getSlackToken() {
+        return slackToken;
+    }
+
+    @Override
+    public Bot getSlackBot() {
+        return this;
+    }
+
+    @Controller(events = {EventType.DIRECT_MENTION, EventType.DIRECT_MESSAGE})
+    public void onReceiveDM(WebSocketSession session, Event event) {
+        reply(session, event, service.getAnswer(event.getText()));
+    }
+}
